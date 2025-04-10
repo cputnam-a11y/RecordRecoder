4bc14599c9e4f0c1e1a6192d7a06a077ac8cd6b0
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-04-10 12:25:04 -0500

    * add remapping to testmod example

--- src/testMod/java/recordrecoder/test/RecordRecoderTest.java
+++ src/testMod/java/recordrecoder/test/RecordRecoderTest.java
@@ -1,18 +1,25 @@
 package recordrecoder.test;
 
+import net.fabricmc.loader.api.FabricLoader;
 import recordrecoder.api.record.ComponentKeyRegistry;
 import recordrecoder.api.record.RecordComponentKey;
 
 public class RecordRecoderTest implements Runnable {
-    public static final RecordComponentKey<String> KEY = RecordComponentKey.create(
-            "addedField",
-            "net.minecraft.entity.damage.DamageRecord",
-            String.class,
-            "Hello, World!"
-    );
+    public static final RecordComponentKey<String> KEY;
 
     @Override
     public void run() {
         ComponentKeyRegistry.INSTANCE.register(KEY);
+    }
+
+    static {
+        var remapper = FabricLoader.getInstance().getMappingResolver();
+        var mappedName = remapper.mapClassName("intermediary", "net.minecraft.class_1281").replace(".", "/");
+        KEY = RecordComponentKey.create(
+                "addedField",
+                mappedName,
+                String.class,
+                "Hello, World!"
+        );
     }
 }
